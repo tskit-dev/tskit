2a816d055356c50fae2c795c8f1f606617852a0c
Jerome Kelleher <jk@well.ox.ac.uk>
2018-02-08 19:27:15 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="tskit",
     version="0.0.0",
     description="The tree sequence toolkit",
+    long_description="**HOLDING PAGE**; real package coming soon!",
     url="https://github.com/tskit-dev/tskit",
     author="The tskit developers",
     author_email="jerome.kelleher@well.ox.uk",
