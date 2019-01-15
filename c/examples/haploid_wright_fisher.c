4201cc0057031dc31a1547a12b01cfc185e17d57
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-15 20:50:18 +0000

    Some improvements to the example WF simulator.

--- c/examples/haploid_wright_fisher.c
+++ c/examples/haploid_wright_fisher.c
@@ -2,53 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <err.h>
 
 #include <gsl/gsl_rng.h>
 
 #include "tsk_tables.h"
 
-/* TODO:
- * Add command line args to get N, T and simplify interval
- * Add some error checking.
- */
+
+#define check_error(val) if (val < 0) {\
+    errx(EXIT_FAILURE, "line %d: %s", __LINE__, tsk_strerror(val));\
+}
 
 void
 simulate(tsk_tbl_collection_t *tables, int N, int T, int simplify_interval, gsl_rng *rng)
 {
     tsk_id_t *buffer[2], *parents, *children, child, left_parent, right_parent;
     double breakpoint;
-    int j, t, b;
+    int ret, j, t, b;
 
-    assert(T % simplify_interval == 0);
-    buffer[0] = malloc(N * sizeof(tsk_id_t));
+    buffer[0] = malloc(N * sizeof(tsk_id_t)); /* TODO change to 1 malloc and check value */
     buffer[1] = malloc(N * sizeof(tsk_id_t));
     b = 0;
     parents = buffer[b];
 
     tables->sequence_length = 1.0;
     for (j = 0; j < N; j++) {
-        parents[j] = tsk_node_tbl_add_row(tables->nodes, 1, T,
+        parents[j] = tsk_node_tbl_add_row(tables->nodes, TSK_NODE_IS_SAMPLE, T,
                 TSK_NULL, TSK_NULL, NULL, 0);
+        check_error(parents[j]);
     }
-    for (t = 0; t < T; t++) {
+    for (t = T - 1; t >= 0; t--) {
         children = buffer[(b + 1) % 2];
         for (j = 0; j < N; j++) {
-            child = tsk_node_tbl_add_row(tables->nodes, 1, T - t - 1,
-                TSK_NULL, TSK_NULL, NULL, 0);
+            child = tsk_node_tbl_add_row(tables->nodes, TSK_NODE_IS_SAMPLE, t,
+                    TSK_NULL, TSK_NULL, NULL, 0);
+            check_error(child);
             left_parent = parents[gsl_rng_uniform_int(rng, N)];
             right_parent = parents[gsl_rng_uniform_int(rng, N)];
             do {
                 breakpoint = gsl_rng_uniform(rng);
             } while (breakpoint == 0); /* tiny proba of breakpoint being 0 */
-            tsk_edge_tbl_add_row(tables->edges, 0, breakpoint, left_parent, child);
-            tsk_edge_tbl_add_row(tables->edges, breakpoint, 1, right_parent, child);
+            ret = tsk_edge_tbl_add_row(tables->edges, 0, breakpoint, left_parent, child);
+            check_error(ret);
+            ret = tsk_edge_tbl_add_row(tables->edges, breakpoint, 1, right_parent, child);
+            check_error(ret);
             children[j] = child;
         }
         parents = children;
         b = (b + 1) % 2;
-        if (((t + 1) % simplify_interval) == 0) {
-            tsk_tbl_collection_sort(tables, 0, 0); /* FIXME; should take position. */
-            tsk_tbl_collection_simplify(tables, parents, N, 0, NULL);
+        if (t % simplify_interval == 0) {
+            ret = tsk_tbl_collection_sort(tables, 0, 0); /* FIXME; should take position. */
+            check_error(ret);
+            ret = tsk_tbl_collection_simplify(tables, parents, N, 0, NULL);
+            check_error(ret);
             for (j = 0; j < N; j++) {
                 parents[j] = j;
             }
@@ -64,8 +70,11 @@ main(int argc, char **argv)
     tsk_tbl_collection_t tables;
     gsl_rng *rng = gsl_rng_alloc(gsl_rng_default);
 
+    if (argc != 5) {
+        errx(EXIT_FAILURE, "usage: N T simplify-interval output-file");
+    }
     tsk_tbl_collection_alloc(&tables, 0);
-    simulate(&tables, 10, 10, 10, rng);
+    simulate(&tables, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), rng);
     tsk_tbl_collection_dump(&tables, "tmp.trees", 0);
     tsk_tbl_collection_free(&tables);
     gsl_rng_free(rng);
