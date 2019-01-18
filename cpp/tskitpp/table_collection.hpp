8954738aa3014b197b35e9222d93d36348bc7c06
molpopgen <krthornt@uci.edu>
2019-01-17 17:25:29 -0800

    Add tskitpp/table_collection.hpp

---
+++ cpp/tskitpp/table_collection.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file table_collection.hpp
+ * @brief C++ wrapper for tsk_table_collection_t
+ */
+#ifndef TSK_CPP_TABLE_COLLECTION_HPP
+#define TSK_CPP_TABLE_COLLECTION_HPP
+
+#include <tskit/tables.h>
+#include <memory>
+
+namespace tskit
+{
+    /**
+    @brief A wrapper around tsk_table_collection_t
+
+    The type is move-only, which ensures safety because
+    tsk_table_collection_t contains bare C pointers.  Thus, 
+    disabling default copy operations on the C++ side is desired.
+
+    TODO: need to mention how to copy
+    */
+    struct table_collection
+    {
+        /** @brief Smart pointer holding the tsk_table_collection_t */
+        std::unique_ptr<tsk_table_collection_t> tables;
+
+        /** @brief Constructor
+        TODO: should this be initalized or not?
+        */
+        table_collection() : tables(new tsk_table_collection_t{}) {}
+
+        inline tsk_table_collection_t* operator->() { return tables.get(); }
+
+        inline const tsk_table_collection_t* operator->() const
+        {
+            return tables.get();
+        }
+
+        inline tsk_table_collection_t*
+        get()
+        {
+            return tables.get();
+        }
+
+        inline const tsk_table_collection_t*
+        get() const
+        {
+            return tables.get();
+        }
+
+        ~table_collection() { tsk_table_collection_free(tables.get()); }
+    };
+} // namespace tskit
+
+#endif
+
