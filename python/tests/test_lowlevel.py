6d9c3d598ee786645064bf6371f36c86188c7657
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-22 12:48:06 +0000

    Update tests for kastore 1.0.0.
    Also change to using meson native coverage handling.

--- python/tests/test_lowlevel.py
+++ python/tests/test_lowlevel.py
@@ -923,7 +923,7 @@ class TestModuleFunctions(unittest.TestCase):
     """
     def test_kastore_version(self):
         version = _tskit.get_kastore_version()
-        self.assertEqual(version, (0, 1, 0))
+        self.assertEqual(version, (1, 0, 0))
 
     def test_tskit_version(self):
         version = _tskit.get_tskit_version()
