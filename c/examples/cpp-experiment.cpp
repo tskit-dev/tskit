9dd160c2915cfd6fe9118596a652ed82fde986cb
molpopgen <krthornt@uci.edu>
2019-01-22 12:36:57 -0800

    Add copy constructor for NodeTable.

--- c/examples/cpp-experiment.cpp
+++ c/examples/cpp-experiment.cpp
@@ -26,19 +26,39 @@ class NodeTable
     private:
         tsk_node_table_t *table;
         const bool allocated_locally;
+        using node_table_ptr = std::unique_ptr<tsk_node_table_t,void(*)(tsk_node_table_t*)>;
+
+        node_table_ptr make_empty_table()
+        {
+            node_table_ptr t(new tsk_node_table_t{},[](tsk_node_table_t* nt){delete nt;});
+            if (t.get() == NULL) {
+                throw std::runtime_error("Out of memory");
+            }
+            return t;
+        }
 
         tsk_node_table_t* allocate(tsk_node_table_t * the_table)
         {
             if (the_table != nullptr){ return the_table; }
 
-            using node_table_ptr = std::unique_ptr<tsk_node_table_t,void(*)(tsk_node_table_t*)>;
-            node_table_ptr t(new tsk_node_table_t{},[](tsk_node_table_t* nt){delete nt;});
-            if (t.get() == NULL) {
-                throw std::runtime_error("Out of memory");
-            }
+            node_table_ptr t(make_empty_table());
             int ret = tsk_node_table_init(t.get(), 0);
             // NOTE: check error is dangerous here.  If you don't
             // use a smart pointer above, you will get a leak.
+            check_error(ret);
+            return t.release();
+        }
+
+        tsk_node_table_t*copy_construct_details(const NodeTable & other)
+        {
+            if(other.allocated_locally == false)
+            {
+                // non-owning, so we share the pointer
+                // to data
+                return other.table;
+            }
+            node_table_ptr t(make_empty_table());
+            int ret = tsk_node_table_copy(other.table, t.get(), 0);
             check_error(ret);
             return t.release();
         }
@@ -47,6 +67,10 @@ class NodeTable
         explicit NodeTable(tsk_node_table_t *the_table) : table(allocate(the_table)), allocated_locally(the_table != table)
         {
             std::cout << "table constructor " << endl;
+        }
+
+        NodeTable(const NodeTable & other) : table(copy_construct_details(other)), allocated_locally(other.allocated_locally)
+        {
         }
 
         ~NodeTable()
@@ -142,6 +166,9 @@ main()
     nodes.add_row(0, 1.0);
     nodes.add_row(0, 2.0);
     std::cout << "Straight table: num_rows = " << nodes.get_num_rows() << endl;
+
+    auto nodes_copy(nodes);
+    assert(nodes.get_num_rows() == nodes_copy.get_num_rows());
 
     TableCollection tables(10);
     std::cout << "Sequence length = " << tables.get_sequence_length() << endl;
