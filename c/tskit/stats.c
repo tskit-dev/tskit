a38e61e03e6d3bf648041929dda2a9139f75f836
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-24 20:48:25 +0000

    Copied copyright notices from msprime C code.

--- c/tskit/stats.c
+++ c/tskit/stats.c
@@ -2,6 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2019 Tskit Developers
+ * Copyright (c) 2016-2017 University of Oxford
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
