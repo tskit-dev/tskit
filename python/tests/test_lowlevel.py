69e621db621e39dcd79925b858c47c8d0475de18
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-24 20:41:16 +0000

    Copied copyright notices from msprime Python headers

--- python/tests/test_lowlevel.py
+++ python/tests/test_lowlevel.py
@@ -1,6 +1,7 @@
 # MIT License
 #
 # Copyright (c) 2018-2019 Tskit Developers
+# Copyright (c) 2015-2018 University of Oxford
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
