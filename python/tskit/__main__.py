0785146afbe9d88c7bca7eb39b6b798ebe6e489d
Jerome Kelleher <jk@well.ox.ac.uk>
2019-02-01 14:48:39 +0000

    Added stub main entry point.
    Closes #112

---
+++ python/tskit/__main__.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from __future__ import division
+
+
+def main():
+    print("Not implemented yet; please be patient!!")
+
+
+if __name__ == "__main__":
+    main()
