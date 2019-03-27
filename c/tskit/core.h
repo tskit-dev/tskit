de26a6c4501a2312b4080c61f7e0bc36298b8083
Jerome Kelleher <jk@well.ox.ac.uk>
2019-03-27 12:37:15 +0000

    Catch table overflows.
    Closes #157

--- c/tskit/core.h
+++ c/tskit/core.h
@@ -189,6 +189,8 @@ of tskit.
 #define TSK_ERR_BAD_TABLE_POSITION                                  -700
 #define TSK_ERR_BAD_SEQUENCE_LENGTH                                 -701
 #define TSK_ERR_TABLES_NOT_INDEXED                                  -702
+#define TSK_ERR_TABLE_OVERFLOW                                      -703
+#define TSK_ERR_COLUMN_OVERFLOW                                     -704
 
 /* Limitations */
 #define TSK_ERR_ONLY_INFINITE_SITES                                 -800
