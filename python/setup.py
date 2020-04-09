b7e66be5e480f23ec88d5abd7f60ea9b2ae94c7b
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2020-04-09 12:33:37 +0100

    Update setup.py to py3.6 minimum version, add 3.7, 3.8 to CI

--- python/setup.py
+++ python/setup.py
@@ -80,16 +80,16 @@ setup(
     version=tskit_version,
     # TODO setup a tskit developers email address.
     author_email="jerome.kelleher@well.ox.ac.uk",
-    python_requires=">=3.4",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="tree sequence",
