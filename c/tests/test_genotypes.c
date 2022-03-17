2c1fdfdea66f79b8e96a1bb2ed985054df6c906d
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-03-17 12:52:08 +0000

    Use id for site param

--- c/tests/test_genotypes.c
+++ c/tests/test_genotypes.c
@@ -874,7 +874,7 @@ test_single_tree_silent_mutations(void)
 }
 
 static void
-test_multiple_tree_get_variant(void)
+test_multiple_variant_decode(void)
 {
     int ret = 0;
     tsk_size_t k;
@@ -933,7 +933,7 @@ test_multiple_tree_get_variant(void)
 }
 
 static void
-test_tree_get_variant_errors(void)
+test_variant_decode_errors(void)
 {
     int ret = 0;
     tsk_treeseq_t ts;
@@ -946,6 +946,13 @@ test_tree_get_variant_errors(void)
     /* Bad samples */
     ret = tsk_variant_init(&var, &ts, bad_samples, 3, NULL, 0);
     CU_ASSERT_EQUAL_FATAL(ret, TSK_ERR_OUT_OF_BOUNDS);
+    tsk_variant_free(&var);
+
+    /* Site out of bounds */
+    ret = tsk_variant_init(&var, &ts, NULL, 0, NULL, 0);
+    CU_ASSERT_EQUAL_FATAL(ret, 0);
+    ret = tsk_variant_decode(&var, 42, 0);
+    CU_ASSERT_EQUAL_FATAL(ret, TSK_ERR_SITE_OUT_OF_BOUNDS);
     tsk_variant_free(&var);
 
     tsk_treeseq_free(&ts);
@@ -970,8 +977,8 @@ main(int argc, char **argv)
         { "test_single_tree_subsample", test_single_tree_subsample },
         { "test_single_tree_many_alleles", test_single_tree_many_alleles },
         { "test_single_tree_silent_mutations", test_single_tree_silent_mutations },
-        { "test_multiple_tree_get_variant", test_multiple_tree_get_variant },
-        { "test_tree_get_variant_errors", test_tree_get_variant_errors },
+        { "test_multiple_variant_decode", test_multiple_variant_decode },
+        { "test_variant_decode_errors", test_variant_decode_errors },
         { NULL, NULL },
     };
 
