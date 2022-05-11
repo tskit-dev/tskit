d69f956fc96c053c243f83ed0b59e6915f0ebcf8
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-05-11 15:27:39 +0100

    Beta release prep

--- python/tests/test_lowlevel.py
+++ python/tests/test_lowlevel.py
@@ -3627,7 +3627,7 @@ class TestModuleFunctions:
 
     def test_tskit_version(self):
         version = _tskit.get_tskit_version()
-        assert version == (0, 99, 15)
+        assert version == (1, 0, 0)
 
     def test_tskit_version_file(self):
         maj, min_, patch = _tskit.get_tskit_version()
