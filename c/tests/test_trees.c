6aaa04b3046fd6efa05e73d4694f1f6d915e35b9
Jerome Kelleher <jk@well.ox.ac.uk>
2022-06-17 11:18:26 +0100

    Simplify population semantics in split_edges
    Closes #2334

--- c/tests/test_trees.c
+++ c/tests/test_trees.c
@@ -7576,25 +7576,6 @@ test_split_edges_no_populations(void)
     }
     tsk_treeseq_free(&split_ts);
 
-    /* And again with imputed population value */
-    ret = tsk_treeseq_split_edges(&ts, time, 1234, 0, metadata, strlen(metadata),
-        TSK_SPLIT_EDGES_IMPUTE_POPULATION, &split_ts);
-    CU_ASSERT_EQUAL_FATAL(ret, 0);
-    CU_ASSERT_EQUAL(tsk_treeseq_get_num_trees(&split_ts), 3);
-    CU_ASSERT_EQUAL(tsk_treeseq_get_num_nodes(&split_ts), 12);
-
-    for (j = 0; j < num_new_nodes; j++) {
-        ret = tsk_treeseq_get_node(&split_ts, new_nodes[j], &node);
-        CU_ASSERT_EQUAL_FATAL(ret, 0);
-        CU_ASSERT_EQUAL(node.time, time);
-        CU_ASSERT_EQUAL(node.flags, 1234);
-        CU_ASSERT_EQUAL(node.individual, TSK_NULL);
-        CU_ASSERT_EQUAL(node.population, TSK_NULL);
-        CU_ASSERT_EQUAL(node.metadata_length, strlen(metadata));
-        CU_ASSERT_EQUAL(strncmp(node.metadata, metadata, strlen(metadata)), 0);
-    }
-    tsk_treeseq_free(&split_ts);
-
     tsk_table_collection_free(&tables);
     tsk_treeseq_free(&ts);
 }
@@ -7639,16 +7620,6 @@ test_split_edges_populations(void)
         CU_ASSERT_EQUAL_FATAL(ret, 0);
         CU_ASSERT_EQUAL(node.population, population);
         tsk_treeseq_free(&split_ts);
-
-        ret = tsk_treeseq_split_edges(&ts, time, 0, population, NULL, 0,
-            TSK_SPLIT_EDGES_IMPUTE_POPULATION, &split_ts);
-        CU_ASSERT_EQUAL_FATAL(ret, 0);
-        CU_ASSERT_EQUAL(tsk_treeseq_get_num_trees(&split_ts), 1);
-        CU_ASSERT_EQUAL(tsk_treeseq_get_num_nodes(&split_ts), 3);
-        ret = tsk_treeseq_get_node(&split_ts, 2, &node);
-        CU_ASSERT_EQUAL_FATAL(ret, 0);
-        CU_ASSERT_EQUAL(node.population, 0);
-        tsk_treeseq_free(&split_ts);
     }
 
     tsk_table_collection_free(&tables);
@@ -7691,12 +7662,6 @@ test_split_edges_errors(void)
     for (j = 0; j < num_invalid_pops; j++) {
         population = invalid_pops[j];
         ret = tsk_treeseq_split_edges(&ts, time, 0, population, NULL, 0, 0, &split_ts);
-        CU_ASSERT_EQUAL_FATAL(ret, TSK_ERR_POPULATION_OUT_OF_BOUNDS);
-        tsk_treeseq_free(&split_ts);
-
-        /* We always check population values, even if they aren't used */
-        ret = tsk_treeseq_split_edges(&ts, time, 0, population, NULL, 0,
-            TSK_SPLIT_EDGES_IMPUTE_POPULATION, &split_ts);
         CU_ASSERT_EQUAL_FATAL(ret, TSK_ERR_POPULATION_OUT_OF_BOUNDS);
         tsk_treeseq_free(&split_ts);
     }
