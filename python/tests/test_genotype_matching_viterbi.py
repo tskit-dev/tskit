e9405fd8a7245eb61930e12d68e5ac379ee2d9b1
astheeggeggs <duncan.stuart.palmer@gmail.com>
2022-08-31 16:25:59 +0100

    removed all instances of tree.num_nodes

--- python/tests/test_genotype_matching_viterbi.py
+++ python/tests/test_genotype_matching_viterbi.py
@@ -172,8 +172,8 @@ class LsHmmAlgorithm:
         # Retain the old T_index, because the internal T that's passed up the tree will
         # retain this ordering.
         old_T_index = copy.deepcopy(self.T_index)
-        self.T_index = np.zeros(tree.num_nodes, dtype=int) - 1
-        self.N = np.zeros(tree.num_nodes, dtype=int)
+        self.T_index = np.zeros(self.ts.num_nodes, dtype=int) - 1
+        self.N = np.zeros(self.ts.num_nodes, dtype=int)
         self.T.clear()
 
         # First, create T root.
@@ -342,7 +342,7 @@ class LsHmmAlgorithm:
                     vt.tree_node = -1
                 vt.value_index = -1
 
-        self.N = np.zeros(self.tree.num_nodes, dtype=int)
+        self.N = np.zeros(self.ts.num_nodes, dtype=int)
         node_map = {st.tree_node: st for st in self.T}
 
         for u in self.tree.samples():
