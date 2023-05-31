f095a761c841346f37188e269873b49b8da799ee
Kevin R. Thornton <krthornt@uci.edu>
2023-05-31 10:36:11 -0700

    add death rate, update lots of the new example internals

--- c/examples/efficient_forward_simulation.c
+++ c/examples/efficient_forward_simulation.c
@@ -97,35 +97,54 @@ edge_buffer_clear(edge_buffer *buffer)
 }
 
 void
-simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
+simulate(
+    tsk_table_collection_t *tables, int N, int T, int simplify_interval, double pdeath)
 {
-    tsk_id_t *buffer, *parents, *children, child, left_parent, right_parent;
+    tsk_id_t *buffer, *alive, *deaths, *replacements, *idmap, child, left_parent,
+        right_parent;
     double breakpoint;
-    int ret, j, t, b;
+    int ret, j, t;
     edge_buffer new_births;
+    size_t ndeaths;
     tsk_modular_simplifier_t simplifier;
 
     assert(simplify_interval != 0); // leads to division by zero
+    assert(pdeath > 0.0 && pdeath <= 1.0);
     ret = edge_buffer_init(&new_births, 1000);
     assert(ret == 0);
     buffer = malloc(2 * N * sizeof(tsk_id_t));
     if (buffer == NULL) {
         errx(EXIT_FAILURE, "Out of memory");
     }
-    tables->sequence_length = 1.0;
-    parents = buffer;
-    for (j = 0; j < N; j++) {
-        parents[j]
-            = tsk_node_table_add_row(&tables->nodes, 0, T, TSK_NULL, TSK_NULL, NULL, 0);
-        check_tsk_error(parents[j]);
+    idmap = malloc(N * sizeof(tsk_id_t));
+    if (idmap == NULL) {
+        errx(EXIT_FAILURE, "Out of memory");
     }
-    b = 0;
+    deaths = malloc(N * sizeof(tsk_id_t));
+    if (deaths == NULL) {
+        errx(EXIT_FAILURE, "Out of memory");
+    }
+    tables->sequence_length = 1.0;
+    alive = buffer;
+    replacements = buffer + N;
+    for (j = 0; j < N; j++) {
+        alive[j]
+            = tsk_node_table_add_row(&tables->nodes, 0, T, TSK_NULL, TSK_NULL, NULL, 0);
+        check_tsk_error(alive[j]);
+    }
     for (t = T - 1; t >= 0; t--) {
-        /* Alternate between using the first and last N values in the buffer */
-        parents = buffer + (b * N);
-        b = (b + 1) % 2;
-        children = buffer + (b * N);
+        ndeaths = 0;
         for (j = 0; j < N; j++) {
+            /* NOTE: the use of rand() is discouraged for
+             * research code and proper random number generator
+             * libraries should be preferred.
+             */
+            if (rand() / (1. + RAND_MAX) <= pdeath) {
+                deaths[ndeaths] = j;
+                ++ndeaths;
+            }
+        }
+        for (j = 0; j < ndeaths; j++) {
             child = tsk_node_table_add_row(
                 &tables->nodes, 0, t, TSK_NULL, TSK_NULL, NULL, 0);
             check_tsk_error(child);
@@ -133,8 +152,8 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
              * research code and proper random number generator
              * libraries should be preferred.
              */
-            left_parent = parents[(size_t)((rand() / (1. + RAND_MAX)) * N)];
-            right_parent = parents[(size_t)((rand() / (1. + RAND_MAX)) * N)];
+            left_parent = alive[(size_t)((rand() / (1. + RAND_MAX)) * N)];
+            right_parent = alive[(size_t)((rand() / (1. + RAND_MAX)) * N)];
             do {
                 breakpoint = rand() / (1. + RAND_MAX);
             } while (breakpoint == 0); /* tiny proba of breakpoint being 0 */
@@ -142,13 +161,19 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
                 left_parent, child, &new_births);
             edge_buffer_buffer_birth(breakpoint, 1.0, tables->nodes.time[right_parent],
                 right_parent, child, &new_births);
-            children[j] = child;
+            replacements[j] = child;
+        }
+        /* replace deaths with births */
+        for (j = 0; j < ndeaths; j++) {
+            alive[deaths[j]] = replacements[j];
         }
         if (t % simplify_interval == 0) {
             printf("Simplify at generation %lld: (%lld nodes %lld edges)", (long long) t,
                 (long long) tables->nodes.num_rows, (long long) tables->edges.num_rows);
             edge_buffer_prep_for_simplification(&new_births);
-            ret = tsk_modular_simplifier_init(&simplifier, tables, children, N, 0);
+            idmap
+                = (tsk_id_t *) realloc(idmap, tables->nodes.num_rows * sizeof(tsk_id_t));
+            ret = tsk_modular_simplifier_init(&simplifier, tables, alive, N, 0);
             check_tsk_error(ret);
             j = 0;
             while (j < new_births.size) {
@@ -164,7 +189,7 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
                 ret = tsk_modular_simplifier_merge_ancestors(&simplifier, left_parent);
                 check_tsk_error(ret);
             }
-            ret = tsk_modular_simplifier_finalise(&simplifier, NULL);
+            ret = tsk_modular_simplifier_finalise(&simplifier, idmap);
             check_tsk_error(ret);
             ret = tsk_modular_simplifier_free(&simplifier);
             check_tsk_error(ret);
@@ -174,12 +199,15 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
             printf(" -> (%lld nodes %lld edges)\n", (long long) tables->nodes.num_rows,
                 (long long) tables->edges.num_rows);
             for (j = 0; j < N; j++) {
-                children[j] = j;
+                alive[j] = idmap[alive[j]];
+                assert(alive[j] != TSK_NULL);
             }
             edge_buffer_clear(&new_births);
         }
     }
     free(buffer);
+    free(idmap);
+    free(deaths);
     edge_buffer_free(&new_births);
 }
 
@@ -189,13 +217,13 @@ main(int argc, char **argv)
     int ret;
     tsk_table_collection_t tables;
 
-    if (argc != 6) {
-        errx(EXIT_FAILURE, "usage: N T simplify-interval output-file seed");
+    if (argc != 7) {
+        errx(EXIT_FAILURE, "usage: N T simplify-interval output-file seed pdeath");
     }
     ret = tsk_table_collection_init(&tables, 0);
     check_tsk_error(ret);
     srand((unsigned) atoi(argv[5]));
-    simulate(&tables, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    simulate(&tables, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atof(argv[6]));
     ret = tsk_table_collection_dump(&tables, argv[4], 0);
     check_tsk_error(ret);
 
