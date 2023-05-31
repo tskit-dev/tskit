3dfc9fce14becb1ddd6c63e06b7bef8879454d47
Kevin R. Thornton <krthornt@uci.edu>
2023-05-31 09:22:14 -0700

    skeleton in place.  we have a segfault

--- c/examples/efficient_forward_simulation.c
+++ c/examples/efficient_forward_simulation.c
@@ -35,10 +35,10 @@ typedef struct {
     birth *births;
     tsk_size_t capacity;
     tsk_size_t size;
-} new_edges;
+} edge_buffer;
 
 int
-new_edges_init(new_edges *buffer, tsk_size_t initial_capacity)
+edge_buffer_init(edge_buffer *buffer, tsk_size_t initial_capacity)
 {
     int ret = 0;
     if (initial_capacity == 0) {
@@ -53,7 +53,7 @@ out:
 }
 
 void
-new_edges_realloc(new_edges *buffer)
+edge_buffer_realloc(edge_buffer *buffer)
 {
     if (buffer->size + 1 >= buffer->capacity) {
         buffer->capacity *= 2;
@@ -62,10 +62,10 @@ new_edges_realloc(new_edges *buffer)
 }
 
 void
-new_edges_buffer_birth(double left, double right, double parent_birth_time,
-    tsk_id_t parent, tsk_id_t child, new_edges *buffer)
+edge_buffer_buffer_birth(double left, double right, double parent_birth_time,
+    tsk_id_t parent, tsk_id_t child, edge_buffer *buffer)
 {
-    new_edges_realloc(buffer);
+    edge_buffer_realloc(buffer);
     buffer->births[buffer->size].left = left;
     buffer->births[buffer->size].right = right;
     buffer->births[buffer->size].parent_birth_time = parent_birth_time;
@@ -75,9 +75,15 @@ new_edges_buffer_birth(double left, double right, double parent_birth_time,
 }
 
 void
-new_edges_prep_for_simplification(new_edges *buffer)
+edge_buffer_prep_for_simplification(edge_buffer *buffer)
 {
     qsort(buffer->births, (size_t) buffer->size, sizeof(birth), cmp_birth);
+}
+
+void
+edge_buffer_clear(edge_buffer *buffer)
+{
+    buffer->size = 0;
 }
 
 void
@@ -86,8 +92,12 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
     tsk_id_t *buffer, *parents, *children, child, left_parent, right_parent;
     double breakpoint;
     int ret, j, t, b;
+    edge_buffer new_births;
+    tsk_modular_simplifier_t simplifier;
 
     assert(simplify_interval != 0); // leads to division by zero
+    ret = edge_buffer_init(&new_births, 1000);
+    assert(ret == 0);
     buffer = malloc(2 * N * sizeof(tsk_id_t));
     if (buffer == NULL) {
         errx(EXIT_FAILURE, "Out of memory");
@@ -118,28 +128,55 @@ simulate(tsk_table_collection_t *tables, int N, int T, int simplify_interval)
             do {
                 breakpoint = rand() / (1. + RAND_MAX);
             } while (breakpoint == 0); /* tiny proba of breakpoint being 0 */
-            ret = tsk_edge_table_add_row(
-                &tables->edges, 0, breakpoint, left_parent, child, NULL, 0);
-            check_tsk_error(ret);
-            ret = tsk_edge_table_add_row(
-                &tables->edges, breakpoint, 1, right_parent, child, NULL, 0);
-            check_tsk_error(ret);
+            edge_buffer_buffer_birth(0., breakpoint, tables->nodes.time[left_parent],
+                left_parent, child, &new_births);
+            edge_buffer_buffer_birth(breakpoint, 1.0, tables->nodes.time[right_parent],
+                right_parent, child, &new_births);
+            // ret = tsk_edge_table_add_row(
+            //    &tables->edges, 0, breakpoint, left_parent, child, NULL, 0);
+            // check_tsk_error(ret);
+            // ret = tsk_edge_table_add_row(
+            //    &tables->edges, breakpoint, 1, right_parent, child, NULL, 0);
+            // check_tsk_error(ret);
             children[j] = child;
         }
         if (t % simplify_interval == 0) {
             printf("Simplify at generation %lld: (%lld nodes %lld edges)", (long long) t,
                 (long long) tables->nodes.num_rows, (long long) tables->edges.num_rows);
+            edge_buffer_prep_for_simplification(&new_births);
+            ret = tsk_modular_simplifier_init(&simplifier, tables, children, N, 0);
+            check_tsk_error(ret);
+            j = 0;
+            while (j < new_births.size) {
+                left_parent = new_births.births[j].parent;
+                while (
+                    j < new_births.size && new_births.births[j].parent == left_parent) {
+                    ret = tsk_modular_simplifier_add_edge(&simplifier,
+                        new_births.births[j].left, new_births.births[j].right,
+                        new_births.births[j].parent, new_births.births[j].child);
+
+                    check_tsk_error(ret);
+                    j++;
+                }
+                ret = tsk_modular_simplifier_merge_ancestors(&simplifier, left_parent);
+                check_tsk_error(ret);
+            }
+            ret = tsk_modular_simplifier_finalise(&simplifier, NULL);
+            check_tsk_error(ret);
+            ret = tsk_modular_simplifier_free(&simplifier);
+            check_tsk_error(ret);
             /* Note: Edges must be sorted for simplify to work, and we use a brute force
              * approach of sorting each time here for simplicity. This is inefficient. */
-            ret = tsk_table_collection_sort(tables, NULL, 0);
-            check_tsk_error(ret);
-            ret = tsk_table_collection_simplify(tables, children, N, 0, NULL);
-            check_tsk_error(ret);
+            // ret = tsk_table_collection_sort(tables, NULL, 0);
+            // check_tsk_error(ret);
+            // ret = tsk_table_collection_simplify(tables, children, N, 0, NULL);
+            // check_tsk_error(ret);
             printf(" -> (%lld nodes %lld edges)\n", (long long) tables->nodes.num_rows,
                 (long long) tables->edges.num_rows);
             for (j = 0; j < N; j++) {
                 children[j] = j;
             }
+            edge_buffer_clear(&new_births);
         }
     }
     free(buffer);
