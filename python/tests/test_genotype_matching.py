2a2bfd446b9bb46a96b2e9e947b5ca10004dd4ac
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2023-10-11 23:29:46 +0100

    Add python 3.12 support

--- python/tests/test_genotype_matching.py
+++ python/tests/test_genotype_matching.py
@@ -1,11 +1,42 @@
+# MIT License
+#
+# Copyright (c) 2019-2023 Tskit Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import copy
 import itertools
 
-import lshmm as ls
 import msprime
 import numpy as np
+import pytest
 
 import tskit
+
+# As numba is often delayed after a Python version upgrade, we
+# skip tests if is not available, so that tskit can be released.
+NO_LSHMM = False
+try:
+    import lshmm as ls
+except ModuleNotFoundError:
+    # Raise a warning message
+    NO_LSHMM = True
+
 
 EQUAL_BOTH_HOM = 4
 UNEQUAL_BOTH_HOM = 0
@@ -1312,9 +1343,10 @@ class FBAlgorithmBase(LSBase):
 
 
 class VitAlgorithmBase(LSBase):
-    """Base for viterbi algoritm tests."""
+    """Base for viterbi algorithm tests."""
 
 
+@pytest.mark.skipif(NO_LSHMM, reason="Requires lshmm")
 class TestMirroringDipdict(FBAlgorithmBase):
     """Tests that mirroring the tree sequence and running forwards and backwards
     algorithms give the same log-likelihood of observing the data."""
@@ -1356,6 +1388,7 @@ class TestMirroringDipdict(FBAlgorithmBase):
             self.assertAllClose(F_mirror_matrix, cm_mirror.decode())
 
 
+@pytest.mark.skipif(NO_LSHMM, reason="Requires lshmm")
 class TestForwardDipTree(FBAlgorithmBase):
     """Tests that the tree algorithm computes the same forward matrix as the simple
     method."""
@@ -1382,6 +1415,7 @@ class TestForwardDipTree(FBAlgorithmBase):
             self.assertAllClose(ll, ll_tree)
 
 
+@pytest.mark.skipif(NO_LSHMM, reason="Requires lshmm")
 class TestForwardBackwardTree(FBAlgorithmBase):
     """Tests that the tree algorithm computes the same forward matrix as the simple
     method."""
@@ -1434,6 +1468,7 @@ class TestForwardBackwardTree(FBAlgorithmBase):
             self.assertAllClose(ll, ll_tree)
 
 
+@pytest.mark.skipif(NO_LSHMM, reason="Requires lshmm")
 class TestTreeViterbiDip(VitAlgorithmBase):
     """
     Test that we have the same log-likelihood between tree and matrix
