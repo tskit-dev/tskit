c9ad6338f3a87189814788d42ddc5243e9073c67
lkirk <kirklloyd@gmail.com>
2026-03-20 17:13:01 -0500

    Add some minimal documentation about the purpose of the two entrypoints

--- c/tskit/trees.c
+++ c/tskit/trees.c
@@ -3384,6 +3384,7 @@ out:
     return ret;
 }
 
+/* Called directly by C python interface `two_locus_count_stat` */
 int
 tsk_treeseq_two_locus_count_general_stat(const tsk_treeseq_t *self,
     tsk_size_t num_sample_sets, const tsk_size_t *sample_set_sizes,
@@ -3439,7 +3440,6 @@ tsk_treeseq_two_locus_count_general_stat(const tsk_treeseq_t *self,
         ret = tsk_treeseq_two_site_count_stat(self, state_dim, num_sample_sets,
             sample_set_sizes, sample_sets, result_dim, f, f_params, norm_f, out_rows,
             row_sites, out_cols, col_sites, options, result);
-        goto out;
     } else {
         tsk_bug_assert(stat_branch);
         ret = check_positions(
@@ -3460,6 +3460,7 @@ out:
     return ret;
 }
 
+/* Called by summary functions implemented in C */
 int
 tsk_treeseq_two_locus_count_stat(const tsk_treeseq_t *self, tsk_size_t num_sample_sets,
     const tsk_size_t *sample_set_sizes, const tsk_id_t *sample_sets,
